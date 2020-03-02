b9dbc98da4808aa33c2c1cd5afd01ec7e0c08fb1
Benjamin C C Hume <didillysquat@gmail.com>
2020-03-02 15:57:51 +0300

    consolidated seq output tables code written

--- output_tables.py
+++ output_tables.py
@@ -3,7 +3,7 @@ import compress_pickle
 import sys
 from base_18s import EighteenSBase
 import pandas as pd
-
+from collections import defaultdict
 
 class EighteenSOutputTables(EighteenSBase):
     """ Class to produce the three output tables
@@ -20,17 +20,41 @@ class EighteenSOutputTables(EighteenSBase):
         # Now get a master list of the sequences in order of cummulative abundance
         self.master_seq_abund_order_list = self._make_master_seq_abund_order_list()
         self.raw_seq_abund_path = os.path.join(self.output_dir, 'raw_seq_abund.csv')
-        self.tax_df_path = os.path.join(self.output_dir, 'tax_annotation.csv')
+
         # Now popualte the dictionary that will be used to create the abundance df
-        self.abundance_df_dict = self._populate_abundance_df_dict()
-        self.abundance_df = None
+        self.abundance_df_dict = {}
+        self._populate_abundance_df_dict()
+
         self.tax_annotation_df_dict = self._populate_tax_annotation_df_dict()
-        self.tax_df = None
+        self.tax_df_path = os.path.join(self.output_dir, 'tax_annotation.csv')
+
+        # Dict for collecting the sequencing information for making the host only consolidated sequences
+        # absolute abundance dataframe
+        self.host_only_master_seq_info_dict = self._populate_host_only_master_seq_info_dict()
+        self.host_only_master_seq_abund_order_list = self._make_host_only_master_seq_abund_order_list()
+        self.consolidated_df_dict = {}
+        self._populated_consolidated_df_dict()
+
+    def make_and_write_raw_abund_output_table(self):
+        # Here we have the self.abundance_df_dict populated and we can now create the dataframe from this dict
+        pd.DataFrame.from_dict(
+            self.abundance_df_dict,
+            orient='index',
+            columns=self.master_seq_abund_order_list
+        ).to_csv(os.path.join(self.output_dir, 'raw_seq_abund.csv'), index=True, columns=True)
 
     def make_and_write_tax_output_table(self):
-        self.tax_df = pd.DataFrame.from_dict(self.abundance_df_dict, orient='index',
-                                                   columns=self.master_seq_abund_order_list)
-        self.abundance_df.to_csv(self.raw_seq_abund_path, index=True, columns=True)
+        pd.DataFrame.from_dict(
+            self.abundance_df_dict,
+            orient='index',
+            columns=self.master_seq_abund_order_list
+        ).to_csv(os.path.join(self.output_dir, 'tax_annotation.csv'), index=True, columns=True)
+
+    def make_and_write_consolidated_host_output_table(self):
+        pd.DataFrame.from_dict(
+            self.consolidated_df_dict, orient='index',
+            columns=self.host_only_master_seq_abund_order_list
+        ).to_csv(os.path.join(self.output_dir, 'consolidated_host.csv'))
 
     def _populate_tax_annotation_df_dict(self):
         # The key should be sequence and the list should be order, family genus, in that order
@@ -40,14 +64,8 @@ class EighteenSOutputTables(EighteenSBase):
             tax_annotation_df_dict[seq] = [annotation_tup[2], annotation_tup[1], annotation_tup[3]]
         return tax_annotation_df_dict
 
-    def make_and_write_raw_abund_output_table(self):
-        # Here we have the self.abundance_df_dict populated and we can now create the dataframe from this dict
-        self.abundance_df = pd.DataFrame.from_dict(self.abundance_df_dict, orient='index',
-                                                   columns=self.master_seq_abund_order_list)
-        self.abundance_df.to_csv(self.raw_seq_abund_path, index=True, columns=True)
-
     def _populate_abundance_df_dict(self):
-        print('Collecting sequence information: second pass')
+        print('Collecting sequence information (original_seqs): second pass')
         for sample_name in self.info_df.index:
             # read in the fasta file
             fasta_file = self._read_in_fasta_file(sample_name)
@@ -65,12 +83,90 @@ class EighteenSOutputTables(EighteenSBase):
             temp_abund_list = [seq_to_abs_abund_dict[seq] if seq in seq_to_abs_abund_dict else 0 for seq in self.master_seq_abund_order_list]
             self.abundance_df_dict[sample_name] = temp_abund_list
 
+    def _populate_host_only_master_seq_info_dict(self):
+        # To populate the consolidated count table that is just the
+        # sequences found in a sample that are the same genus as the most abundant host sequence
+        # We should do similar to before and do two passes
+        # First pass we can just use the consolidated_host_seqs_abund_dict that is found in every
+        # sample directory. And collect this into the master collection dict
+        # For the second pass we need to get an absolute abundance. This is a little trickier.
+        # We basically need to create a reverse dictionary. Each consolidated sequence may be representative
+        # of several original sequence. We will go through each fasta to see if it has a consolidated sequence
+        # representative. If it does or if it doesn't add this to the reverse dict that will be a default dict
+        # Once we have the revese dict populated we can go seq by seq in the consolidated_host_seqs_abund_dict
+        # and summate the original seqs that the given consolidated seq represents and add this information
+        # to the list in the order of the master ordered consolidated sequences.
+
+        host_only_master_seq_info_dict = defaultdict(float)
+        print('Collecting sequence information (consolidated seqs): first pass')
+        for sample_name in self.info_df.index:
+            sys.stdout.write(f'\r{sample_name}')
+
+            # Dict that is sequence key to relative abundance in the sample (of only the given genus sequences)
+            # I.e. dict adds up to one. We will use this only for the keys
+            # to see which seqs we should be concerned with
+            consolidated_host_seqs_abund_dict = compress_pickle.load(
+                os.path.join(self.qc_dir, sample_name, 'consolidated_host_seqs_abund_dict.p.bz'))
+
+            for seq, rel_abund in consolidated_host_seqs_abund_dict.items():
+                host_only_master_seq_info_dict[seq] += rel_abund
+        return host_only_master_seq_info_dict
+
+    def _populated_consolidated_df_dict(self):
+        # For the second pass we need to get an absolute abundance. This is a little trickier.
+        # We basically need to create a reverse dictionary. Each consolidated sequence may be representative
+        # of several original sequence. We will go through each fasta to see if it has a consolidated sequence
+        # representative. If it does or if it doesn't add this to the reverse dict that will be a default dict
+        # Once we have the revese dict populated we can go seq by seq in the consolidated_host_seqs_abund_dict
+        # and summate the original seqs that the given consolidated seq represents and add this information
+        # to the list in the order of the master ordered consolidated sequences.
+        print('Collecting sequence information (consolidated seqs): second pass')
+        coral_blasted_seq_to_consolidated_seq_dict = compress_pickle.load(
+            os.path.join(self.cache_dir, 'coral_blasted_seq_to_consolidated_seq_dict.p.bz'))
+        for sample_name in self.info_df.index:
+            sys.stdout.write(f'\r{sample_name}')
+            # read in the fasta file
+            fasta_file = self._read_in_fasta_file(sample_name)
+
+            # read in the name file and make an abundance dictionary
+            name_rel_abund_dict = self._make_rel_abund_dict_from_names_path(sample_name)
+
+            # the dict we are making for each sample that maps consolidated sequence to the
+            # original sequences it represents
+            consol_seq_to_orig_seq_list = defaultdict(list)
+
+            for i in range(0, len(fasta_file), 2):
+                seq = fasta_file[i+1]
+                try:
+                    # If there is a representative seq for this seq, then log it
+                    consol_seq_to_orig_seq_list[coral_blasted_seq_to_consolidated_seq_dict[seq]].append(seq)
+                except KeyError:
+                    # else just map it to its self in the dict
+                    consol_seq_to_orig_seq_list[seq].append(seq)
+
+            consolidated_host_seqs_abund_dict = compress_pickle.load(
+                os.path.join(self.qc_dir, sample_name, 'consolidated_host_seqs_abund_dict.p.bz'))
+
+            temp_abund_list = []
+            for master_consolidated_seq in self.host_only_master_seq_abund_order_list:
+                if master_consolidated_seq in consolidated_host_seqs_abund_dict:
+                    # Get the summation of the abundances for the represented sequences
+                    temp_abund_list.append(sum([name_rel_abund_dict[repped_seq] for repped_seq in consol_seq_to_orig_seq_list[master_consolidated_seq]]))
+                else:
+                    temp_abund_list.append(0)
+            self.consolidated_df_dict[sample_name] = temp_abund_list
+
+    def _make_host_only_master_seq_abund_order_list(self):
+        return [tup[0] for tup in sorted([_ for _ in self.host_only_master_seq_info_dict.items()],
+                                                                     key=lambda x: x[1],
+                                                                     reverse=True)]
+
     def _make_master_seq_abund_order_list(self):
-        self.master_seq_abund_order_list = [tup[0] for tup in sorted([_ for _ in self.master_seq_info_dict.items()],
+        return [tup[0] for tup in sorted([_ for _ in self.master_seq_info_dict.items()],
                                                                      key=lambda x: x[1]['cummulative_abund'],
                                                                      reverse=True)]
     def _populate_master_seq_info_dict(self):
-        print('Collecting sequence information: first pass')
+        print('Collecting sequence information (original seqs): first pass')
         for sample_name in self.info_df.index:
             sys.stdout.write(f'\r{sample_name}')
             # read in the fasta file
@@ -114,3 +210,8 @@ class EighteenSOutputTables(EighteenSBase):
         tot = sum(abs_abund_dict.values())
         return {seq_name: abund/tot for seq_name, abund in abs_abund_dict.items()}
 
+if __name__ == "__main__":
+    ot = EighteenSOutputTables()
+    ot.make_and_write_raw_abund_output_table()
+    ot.make_and_write_tax_output_table()
+    ot.make_and_write_consolidated_host_output_table()
