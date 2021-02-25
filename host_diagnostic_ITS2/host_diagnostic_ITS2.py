dd9e9a971ed0d2c22f1d14b0439aafec1e00de26
didillysquat <didillysquat@gmail.com>
2021-02-25 11:12:03 +0100

    running diagnostic seqs using pre_med

--- host_diagnostic_ITS2/host_diagnostic_ITS2.py
+++ host_diagnostic_ITS2/host_diagnostic_ITS2.py
@@ -7,16 +7,14 @@ Perhaps easiest to produce a multiway venn for this.
 
 import pandas as pd
 import re
+from collections import defaultdict
+# NB this had to be installed with pip install venn. Couldn't find a conda install
+from venn import venn
+import matplotlib.pyplot as plt
 
 class HostDiagnosticZooxs:
-    def __init__(self):
-        self.path_to_zooxs_counts_pre_med = "/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/TARA_PACIFIC_METAB_ITS2_v1/coral/pre_med_seqs/TARA_PACIFIC_METAB_ITS2_coral_pre_med_seqs_sequences_absolute_abund_v1.csv"
-        self.path_to_zooxs_counts_post_med = "/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/TARA_PACIFIC_METAB_ITS2_v1/coral/post_med_seqs/TARA_PACIFIC_METAB_ITS2_coral_post_med_seqs_sequences_absolute_abund_and_meta_v1.csv"
-        self.path_to_zooxs_fasta = "/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/TARA_PACIFIC_METAB_ITS2_v1/coral/pre_med_seqs/TARA_PACIFIC_METAB_ITS2_coral_pre_med_seqs_sequences_v1.fasta"
-        # Get zooxs count data
-        self.counts_df = pd.read_csv(self.path_to_zooxs_counts_post_med)
-        self.counts_df = self.counts_df.iloc[:-1,]
-        self.counts_df.index = self.counts_df["sample-id"]
+    def __init__(self, pre_post='post'):
+        self.pre_post = pre_post
         
         # Read in the host group data
         # 'sampling-design_label' 'sample-id_source'
@@ -29,24 +27,134 @@ class HostDiagnosticZooxs:
         poc_golden.index = poc_golden["INDV"]
         # We want to work with the subset of samples for which there is host grouping info and ITS2 sequences
         poc_golden = poc_golden[~pd.isnull(poc_golden["dataset_ITS2_sample_id"])]
-        self.counts_df_with_host = self.counts_df.loc[[_ for _ in self.counts_df.index if _ in poc_golden["dataset_ITS2_sample_id"].values],]
         
-        # For the count table this will likely leave us with a lot of columns where it is only 0s. we will want to get rid of these
-        self.counts_df_with_host = self.counts_df_with_host.loc[:, (self.counts_df_with_host != 0).any(axis=0)]
+        if self.pre_post == 'post':
+            self.path_to_zooxs_counts = "/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/TARA_PACIFIC_METAB_ITS2_v1/coral/post_med_seqs/TARA_PACIFIC_METAB_ITS2_coral_post_med_seqs_sequences_absolute_abund_and_meta_v1.csv"
+            # Get zooxs count data
+            self.counts_df = pd.read_csv(self.path_to_zooxs_counts)
+            self.counts_df = self.counts_df.iloc[:-1,]
+            self.counts_df.index = self.counts_df["sample-id"]
+            self.counts_df_with_host = self.counts_df.loc[[_ for _ in self.counts_df.index if _ in poc_golden["dataset_ITS2_sample_id"].values],]
+            # For the count table this will likely leave us with a lot of columns where it is only 0s. we will want to get rid of these
+            self.counts_df_with_host = self.counts_df_with_host.loc[:, (self.counts_df_with_host != 0).any(axis=0)]
         
+        else:
+            self.path_to_zooxs_counts = "/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/TARA_PACIFIC_METAB_ITS2_v1/coral/pre_med_seqs/TARA_PACIFIC_METAB_ITS2_coral_pre_med_seqs_sequences_absolute_abund_v1.csv"
+            with open(self.path_to_zooxs_counts, 'r') as f:
+                header = None
+                rows = []
+                for i, line in enumerate(f):
+                    if i == 0:
+                        header = line.rstrip().split(',')
+                    else:
+                        split_line_list = line.rstrip().split(',')
+                        if split_line_list[1] in poc_golden["dataset_ITS2_sample_id"].values:
+                            rows.append(split_line_list)
+            self.counts_df_with_host = pd.DataFrame(rows, columns=header)
+            self.counts_df_with_host.index = self.counts_df_with_host["sample-id"]
+            self.counts_df_with_host = self.counts_df_with_host.loc[:, (self.counts_df_with_host != 0).any(axis=0)]
+            foo = 'bar'
+
+        
+        # Now strip this down to only the abundances
+        # Get index of first sequence
+        drop_index = 0
+        for i, col in enumerate(list(self.counts_df_with_host)):
+            if col[0] in list('ABCDEFGHI') or col[-1] in list('ABCDEFGHI'):
+                drop_index = i
+                break
+        self.counts_df_with_host = self.counts_df_with_host.iloc[:,drop_index:]
+
         # Need to convert between didiers naming system and the coral sample-id format
         didier_to_sp_name_dict = {k:v for k, v in zip(poc_golden["INDV"].values, poc_golden["dataset_ITS2_sample_id"]) if not pd.isnull(v)}
         self.host_groupings = self.host_groupings.loc[[_ for _ in self.host_groupings.index.values if _ in didier_to_sp_name_dict],]
-        foo = 'bar'
+        self.host_groupings.index = [didier_to_sp_name_dict[_] for _ in self.host_groupings.index.values]
+        self.group_to_sample_list_dict = {k: list(self.host_groupings[self.host_groupings==k].index.values) for k in self.host_groupings.unique() if not pd.isnull(k)}
+
         
     def look_for_diagnostic_sqeuences(self):
-        """ For every sequence in the ITS2 count table, look to see if it is found in every individual
+        """ 
+        For every sequence in the ITS2 count table, look to see if it is found in every individual
             of a given group if it is, then count this sequence. Keep track of which groups a given
             sequence is found in and then we will output this info as a Venn or table. We will obviously
             be particularly interested in those sequences that are only found in one of the groups.
-         """
 
-HostDiagnosticZooxs()
+        Result:
+        In the end there are very few sequences that are found in common with all samples of a host group.
+        This approach will not work. Single sequences cannot be diagnostic.
+        """
+        host_group_to_seq_dd = defaultdict(set)
+        for svd_group, sample_list in self.group_to_sample_list_dict.items():
+            tot = len(list(self.counts_df_with_host))
+            for i, seq in enumerate(list(self.counts_df_with_host)):
+                print(f"{svd_group}:{i}/{tot}")
+                ser = self.counts_df_with_host[seq]
+                ser = ser[ser != 0]
+                if set(set(sample_list)).issubset(ser.index.values):
+                    # Then this sequence is found in all samples of the given host group
+                    host_group_to_seq_dd[svd_group].add(seq)
+        # At this point we know which sequences are found in all samples of a given group
+        # now we can plot this up as a venn
+        venn_obj = venn(host_group_to_seq_dd)
+        venn_obj.set_title("Venn of sequnces found in all\nsamples of a given host group")
+        plt.savefig('/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/venn_plot.png' )
+        
+        # host_group_to_seq_dd = defaultdict(set)
+        # for seq in list(self.counts_df_with_host):
+        #     ser = self.counts_df_with_host[seq]
+        #     ser = ser[ser != 0]
+        #     # Check to see if, of the samples this seq is found in, if at least
+        #     # one of those samples if from one of the host groups
+        #     for svd_group, sample_list in self.group_to_sample_list_dict.items():
+        #         if len(set(ser.index.values).intersection(set(sample_list))) > 1:
+        #             # Then at least one of the samples that this seq is found in is of the host group
+        #             host_group_to_seq_dd[svd_group].add(seq)
+        # venn_obj = venn(host_group_to_seq_dd)
+        # venn_obj.set_title("Venn of sequnces found in all\nsamples of a given host group")
+        # plt.savefig('/home/humebc/projects/tara/tara_full_dataset_processing/host_diagnostic_ITS2/venn_plot.png' )
+        # foo = 'this'
+
+        # Venn is not really right for what we want to show here.
+        # let's just straight up search for what we're after
+        host_group_to_seq_dd = defaultdict(set)
+        for svd_group, sample_list in self.group_to_sample_list_dict.items():
+            for seq in list(self.counts_df_with_host):
+                ser = self.counts_df_with_host[seq]
+                ser = ser[ser != 0]
+                # Check to see if this sequences is found in all samples of this group
+                # and also none of the samples of the other groups
+                if set(set(sample_list)).issubset(ser.index.values):
+                    # Seq is found in all samples of this host group
+                    found_in_other = False
+                    for svd_group_other, sample_list_other in [(k, v) for k, v in self.group_to_sample_list_dict.items() if k != svd_group]:
+                        # For all of the other svd_groups
+                        if len(set(ser.index.values).intersection(set(sample_list_other))) > 0:
+                            found_in_other = True
+                    if found_in_other:
+                        continue
+                    else:
+                        host_group_to_seq_dd[svd_group].add(seq)    
+                else:
+                    continue
+        print("Sequences that are unique diagnostic of the host group:")
+        print(host_group_to_seq_dd)
+        
+    def investigate_diagnostic_clusters(self):
+        """
+        Here we will investigate whether here is some form of structure within the ITS2 samples that would allow us
+        to correlate to the species grouping.
+
+        First thing to do will be to do will be to plot up the samples accordig to similarity
+        and annotate them with the groupings so that we can get an idea of the correlation.
+        It will probably be useful to do with only those ITS2 samples that have a group annotation available
+        and also with all of the samples.
+        """
+        foo = 'bar'
+
+         
+# The look_for_diagnostic_sequences can be run using pre_post='pre' to use the pre_med seqs.
+# However this still doesn't produce any viable diagnostic sequences and takes a lot longer due to the number of sequnces
+HostDiagnosticZooxs(pre_post='pre').look_for_diagnostic_sqeuences()
 
 """
 Code that I was using to check why there were some ITS2 samples missing with relation to the
